8b6efe690337ca90fbb41c31e602d7b64aac4c70
Chaitanya Raju <vchaitanyaraju9@gmail.com>
2026-08-13 15:04:59 +0530

    Update TASK7.java

--- TASK7.java
+++ TASK7.java
@@ -1,31 +1,16 @@
 class Solution {
-    public int[] sortedSquares(int[] nums) {
-        int i=0;
-        int j=nums.length-1;
-        int ls=0;
-        int rs=0;
-        int[] res=new int[nums.length];
-        
-        for(int ind=nums.length-1;ind>=0;ind--){
-            ls=nums[i]*nums[i];
-            rs=nums[j]*nums[j];
-            if(ls>rs){
-                res[ind]=ls;
-                i++;
-
-            }
-            else{
-                res[ind]=rs;
-                j--;
-            }
-
-        }
-        return res;
-        
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        return call(nums,ans,0);
+    }
+    public int[] call(int[] nums, int[] ans,int i){
+        if(i == nums.length)return ans;
+        ans[i] = nums[nums[i]];
+        return call(nums,ans,i+1);
     }
 }
 
-//  OUTPUT 
+//  OUTPUT
 
-Input: nums = [-4,-1,0,3,10]
-Output: [0,1,9,16,100]
+Input: nums = [0,2,1,5,3,4]
+Output: [0,1,2,4,5,3]
