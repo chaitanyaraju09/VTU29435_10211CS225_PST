b28d19376e3b301f1eeed64cbf75bf45c0e5229d
Chaitanya Raju <vchaitanyaraju9@gmail.com>
2026-08-13 15:13:02 +0530

    Create TASK9.java

---
+++ TASK9.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int altitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            altitude += g;
+            maxAltitude = Math.max(maxAltitude, altitude);
+        }
+
+        return maxAltitude;
+    }
+}
+
+
+//  OUTPUT 
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
